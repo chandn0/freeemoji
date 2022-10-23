3829a5927ef4ca9ee67ae2a8c075671f39b3561c
chandn0 <boinapallychandan007@gmail.com>
2022-10-23 10:37:34 +0530

    update logo

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -4,8 +4,8 @@ import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from '../Marketplace.json';
 import { useLocation } from "react-router";
 
-export default function SellNFT () {
-    const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
+export default function SellNFT() {
+    const [formParams, updateFormParams] = useState({ name: '', description: '', price: '' });
     const [fileURL, setFileURL] = useState(null);
     const ethers = require("ethers");
     const [message, updateMessage] = useState('');
@@ -18,21 +18,21 @@ export default function SellNFT () {
         try {
             //upload the file to IPFS
             const response = await uploadFileToIPFS(file);
-            if(response.success === true) {
+            if (response.success === true) {
                 console.log("Uploaded image to Pinata: ", response.pinataURL)
                 setFileURL(response.pinataURL);
             }
         }
-        catch(e) {
+        catch (e) {
             console.log("Error during file upload", e);
         }
     }
 
     //This function uploads the metadata to IPFS
     async function uploadMetadataToIPFS() {
-        const {name, description, price} = formParams;
+        const { name, description, price } = formParams;
         //Make sure that none of the fields are empty
-        if( !name || !description || !price || !fileURL)
+        if (!name || !description || !price || !fileURL)
             return;
 
         const nftJSON = {
@@ -42,12 +42,12 @@ export default function SellNFT () {
         try {
             //upload the metadata JSON to IPFS
             const response = await uploadJSONToIPFS(nftJSON);
-            if(response.success === true){
+            if (response.success === true) {
                 console.log("Uploaded JSON to Pinata: ", response)
                 return response.pinataURL;
             }
         }
-        catch(e) {
+        catch (e) {
             console.log("error uploading JSON metadata:", e)
         }
     }
@@ -77,44 +77,44 @@ export default function SellNFT () {
 
             alert("Successfully listed your NFT!");
             updateMessage("");
-            updateFormParams({ name: '', description: '', price: ''});
+            updateFormParams({ name: '', description: '', price: '' });
             window.location.replace("/")
         }
-        catch(e) {
-            alert( "Upload error"+e )
+        catch (e) {
+            alert("Upload error" + e)
         }
     }
 
     console.log("Working", process.env);
     return (
         <div className="">
-        <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-10" id="nftForm">
-            <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
-            <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3>
-                <div className="mb-4">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">NFT Name</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Axie#4563" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name}></input>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">NFT Description</label>
-                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" cols="40" rows="5" id="description" type="text" placeholder="Axie Infinity Collection" value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}></textarea>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="price">Price (in ETH)</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Min 0.01 ETH" step="0.01" value={formParams.price} onChange={e => updateFormParams({...formParams, price: e.target.value})}></input>
-                </div>
-                <div>
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="image">Upload Image</label>
-                    <input type={"file"} onChange={OnChangeFile}></input>
-                </div>
-                <br></br>
-                <div className="text-green text-center">{message}</div>
-                <button onClick={listNFT} className="font-bold mt-10 w-full bg-purple-500 text-white rounded p-2 shadow-lg">
-                    List NFT
-                </button>
-            </form>
-        </div>
+            <Navbar></Navbar>
+            <div className="flex flex-col place-items-center mt-10" id="nftForm">
+                <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
+                    <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3>
+                    <div className="mb-4">
+                        <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">Emoji Name</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Axie#4563" onChange={e => updateFormParams({ ...formParams, name: e.target.value })} value={formParams.name}></input>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">Emoji Description</label>
+                        <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" cols="40" rows="5" id="description" type="text" placeholder="Axie Infinity Collection" value={formParams.description} onChange={e => updateFormParams({ ...formParams, description: e.target.value })}></textarea>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="price">Price (in ETH)</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Min 0.01 ETH" step="0.01" value={formParams.price} onChange={e => updateFormParams({ ...formParams, price: e.target.value })}></input>
+                    </div>
+                    <div>
+                        <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="image">Upload Emoji</label>
+                        <input type={"file"} onChange={OnChangeFile}></input>
+                    </div>
+                    <br></br>
+                    <div className="text-green text-center">{message}</div>
+                    <button onClick={listNFT} className="font-bold mt-10 w-full bg-purple-500 text-white rounded p-2 shadow-lg">
+                        upload Emoji
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
